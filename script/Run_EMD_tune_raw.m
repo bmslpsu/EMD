7b5f4429fcfc57438b8501a375c1b9d0b0a9d34f
Benjamin Cellini <31375763+boc5244@users.noreply.github.com>
2019-09-27 16:36:17 -0400

    Updates

--- script/Run_EMD_tune_raw.m
+++ script/Run_EMD_tune_raw.m
@@ -17,7 +17,7 @@ imageHeight     = 137;      % height of input visual field
 imageWidth      = 8204;     % width of input visual field
 method          = 'sine';   % spatial form
 
-freq            = logspace(-1,2,50)'; % frequencies to sweep [Hz]
+freq            = logspace(-1,2,100)'; % frequencies to sweep [Hz]
 nf              = length(freq);
 
 delay           = [5 10 20 35 60]*10^(-3); % EMD delays
@@ -34,7 +34,12 @@ for kk = 1:nd
     [Mag(:,kk),Phase(:,kk),R2(:,kk)] = EMD_Tune(self,freq);
 end
 
-%% Magnitude, Phase, R^2 vs freq
+MagN = Mag;
+for kk = 1:nd
+   MagN(:,kk) = Mag(:,kk) ./ max(Mag(:,kk));
+end    
+    
+%% Magnitude, Phase, R^2 vs frequency
 FIG = figure (1) ; clf ; hold on
 FIG.Color = 'w';
 FIG.Units = 'inches';
@@ -44,8 +49,9 @@ clear ax
 Color = jet(nd);
 for kk = 1:nd
     ax(1) = subplot(3,1,1); hold on ; ylabel('Mag')
-        plot(freq,Mag(:,kk), 'Color', Color(kk,:), 'LineWidth', 1.5)
-        [mm,midx] = max(Mag(:,kk));
+        ylim([0 1.2])
+        plot(freq,MagN(:,kk), 'Color', Color(kk,:), 'LineWidth', 1.5)
+        [mm,midx] = max(MagN(:,kk));
         plot([freq(midx) , freq(midx)], [0 , mm], '--', 'Color', Color(kk,:), 'LineWidth',1)
         plot([freq(1) , freq(midx)], [mm , mm], '--', 'Color', Color(kk,:), 'LineWidth',1)
 
@@ -54,11 +60,47 @@ for kk = 1:nd
         plot(freq, rad2deg(Phase(:,kk)), 'Color', Color(kk,:), 'LineWidth', 1.5)
 
     ax(3) = subplot(3,1,3); hold on ; ylabel('r^{2}')
-        % ylim([0 1])
+        ylim([0 1])
         plot(freq,R2(:,kk), 'Color', Color(kk,:),'LineWidth', 1.5)
 end
 set(ax,'FontSize',8,'XScale','log','XLim',[1e-1 1e2],'XGrid','on','YGrid','on')
 linkaxes(ax,'x')
 XLabelHC = get(ax, 'XLabel');
-set([XLabelHC{:}], 'String', 'freq (Hz)')
+set([XLabelHC{:}], 'String', 'Frequency (Hz)')
+
+legend(strcat(string(1000*delay),'ms'))
+
+%% Magnitude, Phase, R^2 vs mean temporal frequency
+FIG = figure (2) ; clf ; hold on
+FIG.Color = 'w';
+FIG.Units = 'inches';
+FIG.Position = [200 200 4 3*2.5];
+movegui(FIG,'center')
+clear ax
+Color = jet(nd);
+A = 15;
+wavelength = 30;
+mean_temp_freq = 4*A*freq./wavelength;
+for kk = 1:nd
+    ax(1) = subplot(3,1,1); hold on ; ylabel('Mag')
+        ylim([0 1.2])
+        plot(mean_temp_freq,MagN(:,kk), 'Color', Color(kk,:), 'LineWidth', 1.5)
+        [mm,midx] = max(MagN(:,kk));
+        plot([mean_temp_freq(midx) , mean_temp_freq(midx)], [0 , mm], '--', 'Color', Color(kk,:), 'LineWidth',1)
+        plot([mean_temp_freq(1) , mean_temp_freq(midx)], [mm , mm], '--', 'Color', Color(kk,:), 'LineWidth',1)
+
+    ax(2) = subplot(3,1,2); hold on ; ylabel(['Phase (' char(176) ')'])
+        ylim([-250 0])
+        plot(mean_temp_freq, rad2deg(Phase(:,kk)), 'Color', Color(kk,:), 'LineWidth', 1.5)
+
+    ax(3) = subplot(3,1,3); hold on ; ylabel('r^{2}')
+        ylim([0 1])
+        plot(mean_temp_freq,R2(:,kk), 'Color', Color(kk,:),'LineWidth', 1.5)
+end
+set(ax,'FontSize',8,'XScale','linear','XLim',[0 25],'XGrid','on','YGrid','on')
+linkaxes(ax,'x')
+XLabelHC = get(ax, 'XLabel');
+set([XLabelHC{:}], 'String', 'Mean Temporal Frequency (Hz)')
+
+legend(strcat(string(1000*delay),'ms'))
 end
